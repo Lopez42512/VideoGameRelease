5d05500b90d3d0f5887ca5f5c6f534392da16d10
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-01-20 22:44:22 -0500

    getting the fetch request working and displaying some video game images and playing the respective trailer once clicked

--- video-game-release/src/App.js
+++ video-game-release/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from "react"
+import YouTube from "react-youtube"
+// npm install react-youtube
+ 
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            number: 1,
+            color: "blue",
+            result: [],
+            videoId: "",
+            click: false
+        }
+    }
+ 
+    handlesubmit = (event) => {
+        event.preventDefault()
+        const { name, value } = event.target
+        console.log(value)
+        // name === "hello" ? this.setState({ number: 0 }) : this.setState({ number: 1, color: "green" })
+        console.log(name)
+        this.setState({ videoId: name, click: !this.state.click })
+    }
+ 
+    componentDidMount() {
+        fetch("https://api.rawg.io/api/games?dates=2018-10-10,2020-10-10&ordering=-added")
+            .then(res => res.json())
+            .then((results) => {
+                this.setState({ result: results.results })
+            })
+    }
+ 
+    render() {
+        console.log(this.state.result)
+        console.log(this.state.click)
+        const gameImg = this.state.result.map((img) => <img onClick={this.handlesubmit} value={img.clip.video} name={img.clip.video} style={{ width: "100%", height: 400 }} key={img.id} src={img.background_image} alt="img not avalable" />)
+        const opts = {
+            height: '390',
+            width: '100%',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                autoplay: 0
+            }
+        };
+        // const display = 
+        //     this.state.click === false ? <div>{gameImg}</div>   : <YouTube
+        //         videoId={this.state.videoId}
+        //         opts={opts}
+        //         onReady={this._onReady}
+        //     />
+        
+        return (
+            <div>
+                {this.state.click === false ? gameImg   : <div><div onClick={this.handlesubmit}>x</div><YouTube
+                videoId={this.state.videoId}
+                opts={opts}
+                onReady={this._onReady}
+            /></div>}
+ 
+            </div>
+        )
+    }
+    _onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
 }
 
-export default App;
+export default App
