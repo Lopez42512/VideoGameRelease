20931ca06ecf994971c81e20ea5c5860395ea5e2
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-02-16 21:22:27 -0500

    images added and can be click to replace video

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ class App extends Component {
       results: [],
       targetGame: [],
       youtubePage: true,
-      overlay: true
+      overlay: true,
+      imageOrVid: true,
+      bigImg: "",
+      gameImg: [
+        "https://media.rawg.io/media/games/fb5/fb5e0fdb1f6bb0e8b5da5d08bb83a5fc.jpg"
+      ]
     };
   }
 
@@ -26,7 +31,10 @@ class App extends Component {
     )
       .then(response => response.json())
       .then(results => {
-        this.setState({ results: results.results });
+        this.setState({
+          results: results.results,
+          targetGame: results.results
+        });
       });
   }
 
@@ -34,14 +42,27 @@ class App extends Component {
     this.setState({ overlay: !this.state.overlay });
   };
 
+  unClick = () => {
+    this.setState({ youtubePage: !this.state.youtubePage, imageOrVid: true });
+  };
+
   handlesubmit = event => {
     const { name } = event.target;
     const grabGame = this.state.results.find(id => id.id === parseInt(name));
     console.log(grabGame);
+    const grabImg = grabGame.short_screenshots;
+
     this.setState({
       targetGame: grabGame,
-      youtubePage: !this.state.youtubePage
+      youtubePage: !this.state.youtubePage,
+      gameImg: grabImg
     });
+  };
+
+  chooseImg = event => {
+    console.log(event)
+    const img = event.target.src;
+    this.setState({ bigImg: img, imageOrVid: false });
   };
 
   render() {
@@ -62,14 +83,22 @@ class App extends Component {
       </div>
     ));
     const opts = {
-      height: "500px",
-      width: "800px",
+      // height: "500px",
+      // width: "800px",
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 0
       }
     };
-
+    const gameImg = this.state.gameImg.map(img => (
+      <img
+        onClick={this.chooseImg}
+        className="scrollImg"
+        src={img.image}
+        alt="not found"
+      />
+    ));
+    
     return (
       <div>
         <Header handleClick={this.handleClick}></Header>
@@ -85,17 +114,35 @@ class App extends Component {
             {games}
           </div>
         ) : (
-          <div className="youtubeContainer">
-            {/* <button style={{position:"absolute", fontSize: 40 }} onClick={this.handlesubmit}>
+          <div>
+            <button
+              style={{ position: "relative", fontSize: 40, left: "0%" }}
+              onClick={this.unClick}
+            >
               x
-            </button> */}
-            <h2 className="gameTitle">{this.state.targetGame.name}</h2>
-            <div className="game">
-              <YouTube
-                videoId={this.state.targetGame.clip.video}
-                opts={opts}
-                onReady={this._onReady}
-              />
+            </button>
+            <div className="youtubeContainer">
+              <h2 className="gameTitle">{this.state.targetGame.name}</h2>
+              <div className="game">
+                {this.state.imageOrVid ? (
+                  <YouTube
+                    className="gameVid"
+                    videoId={this.state.targetGame.clip.video}
+                    opts={opts}
+                    onReady={this._onReady}
+                  />
+                ) : (
+                  <img className="gameVid" src={this.state.bigImg} alt="not found" />
+                )}
+              </div>
+              <div className="scrollmenu">
+                <YouTube
+                  videoId={this.state.targetGame.clip.video}
+                  opts={{ width: 200, height: 100 }}
+                  onReady={this._onReady}
+                />
+                {gameImg}
+              </div>
             </div>
           </div>
         )}
